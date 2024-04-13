d865bf08d3033def03f183e2a56c210b7244e216
thaisparsh <166853024+thaisparsh@users.noreply.github.com>
2024-04-13 09:49:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello chacha /n'
+  const msg = 'Hello Thailasn/n'
   res.end(msg);
 });
 
